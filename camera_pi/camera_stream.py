5c1f840c6b122eb7a94c93a2c7ceda54087de0ef
Giang Nguyen <kdgiang.nguyen02@gmail.com>
2026-02-25 15:17:44 +0100

    Added camera streaming from Raspberry Pi3 to the laptop

--- camera_pi/camera_stream.py
+++ camera_pi/camera_stream.py
@@ -1,2 +1,43 @@
-print("macOS test")
-print("check number two")
+import cv2
+import zmq
+import base64
+import time
+
+def start_stream():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://*:5555") 
+    print("[INFO] ZeroMQ Server started. Broadcasting on port 5555...")
+
+    cap = cv2.VideoCapture(0)
+    
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    time.sleep(2)
+    
+    print("[INFO] Camera warmed up. Streaming video...")
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("[ERROR] Failed to grab frame. Is the USB camera plugged in?")
+                time.sleep(1)
+                continue
+            
+            _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+            
+            jpg_as_text = base64.b64encode(buffer)
+            socket.send(jpg_as_text)
+
+    except KeyboardInterrupt:
+        print("\n[INFO] Stopping stream...")
+        
+    finally:
+        cap.release()
+        socket.close()
+        context.term()
+        print("[INFO] Hardware released safely.")
+
+if __name__ == "__main__":
+    start_stream()
