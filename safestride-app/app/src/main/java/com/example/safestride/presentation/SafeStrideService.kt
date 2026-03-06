82b3dec3ddff29841d6048fce1989c448e8c3e1f
Dimitris Thomopoulos <thomopoulos59@gmail.com>
2026-03-06 13:53:23 +0100

    Update SafeStrideService foreground type and add justification metadata

--- safestride-app/app/src/main/java/com/example/safestride/presentation/SafeStrideService.kt
+++ safestride-app/app/src/main/java/com/example/safestride/presentation/SafeStrideService.kt
@@ -4,6 +4,8 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -29,7 +31,11 @@ class SafeStrideService : Service() {
             .setOngoing(true)
             .build()
 
-        startForeground(1, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
+        } else {
+            startForeground(1, notification)
+        }
 
         // START THE UDP SERVER
         startUdpServer()
