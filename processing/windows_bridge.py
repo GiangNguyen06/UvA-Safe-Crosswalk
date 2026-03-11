62d84d9c7ba65db1c2dfe1faf39a0f6f1778e1bf
GiangNguyen06 <kdgiang.nguyen02@gmail.com>
2026-03-11 22:01:44 +0100

    added network set up

---
+++ processing/windows_bridge.py
@@ -0,0 +1,24 @@
+import socket
+
+# Stand outside and listen to Tailscale on Port 9001
+listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+listen_sock.bind(("0.0.0.0", 9001)) 
+
+# 2.Setup the pipe to the Emulator
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("[BRIDGE] Standing by. Catching Pi packets and shoving them into Emulator...")
+
+try:
+    while True:
+        data, addr = listen_sock.recvfrom(1024)
+        message = data.decode()
+        print(f"[BRIDGE] Caught '{message}' from Pi! Shoving to Emulator...")
+        
+        # Shove it into the Telnet door of the Emulator (localhost:9000)
+        send_sock.sendto(data, ("127.0.0.1", 9000))
+except KeyboardInterrupt:
+    print("\n[BRIDGE] Stopped.")
+finally:
+    listen_sock.close()
+    send_sock.close()
