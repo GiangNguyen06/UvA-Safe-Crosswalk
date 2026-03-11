ac9d8f8ddc22270e6d3d400b3bb641ea3e0d8cb7
GiangNguyen06 <kdgiang.nguyen02@gmail.com>
2026-03-11 14:25:00 +0100

    Arducam update

--- processing/receive_stream.py
+++ processing/receive_stream.py
@@ -14,7 +14,7 @@ env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
 def receive_stream():
-    pi_ip = os.getenv("PI_IP_HOME")
+    pi_ip = os.getenv("PI_IP_WORK")
     port = os.getenv("ZMQ_PORT")
     
     if not pi_ip or not port:
